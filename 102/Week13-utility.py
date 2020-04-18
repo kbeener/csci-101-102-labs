af5aab41d32e4676ff933cefdb4715822617c290
kbeener <63762194+kbeener@users.noreply.github.com>
2020-04-18 15:38:23 -0500

    fixed the '\n' issue

--- 102/Week13-utility.py
+++ 102/Week13-utility.py
@@ -9,7 +9,7 @@ def print_output(string):
 def load_file(filename):
     with open(filename,'r') as f:
         lines = f.readlines()
-        for i in lines:
-            i = i.replace('\n','')
+        for i in range(len(lines)):
+            lines[i] = lines[i].replace('\n','')
         print('OUTPUT',lines)
 
